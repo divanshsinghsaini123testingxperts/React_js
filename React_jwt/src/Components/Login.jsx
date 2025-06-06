7f4c7ff44d6447a52b0f722c2ee51618c14295b1
Divansh Singh <divansh.singh@testingxperts.com>
2025-06-06 12:23:43 +0530

    Till register

---
+++ React_jwt/src/Components/Login.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    
+    const handleLogin = () => {
+        const user = { Email: email, Password : password };
+        const respnse = fetch('https://reqres.in/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Login failed');
+            }
+        })
+        .then(data => {
+            // Handle successful login
+            console.log('Login successful:', data);
+            localStorage.setItem('token', data.token); // Store token in localStorage
+            navigate('/'); // Redirect to home page after login
+        })
+        .catch(error => {
+            // Handle errors
+            console.error('Error:', error);
+        });
+    };
+
+    return (
+        <div className="login-container">
+        <h2>Login</h2>
+        <input 
+            type="email" 
+            placeholder="Email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+        />
+        <input 
+            type="password" 
+            placeholder="Password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+        />
+        <button onClick={handleLogin}>Login</button>
+        </div>
+    );
+
+}
+export default Login;
---
+++ React_jwt/src/Components/Register.jsx
@@ -0,0 +1,74 @@
+
+
+
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+function Register() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [name , setName] = useState('');
+    const navigate = useNavigate();
+
+    const handleRegister = () => {
+        const user = {Id : 0,  Name: name, Email: email, Password: password };
+        if( password !== confirmPassword) {
+            alert("Passwords do not match");
+            return;
+        }
+        fetch('https://reqres.in/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                navigate('/Register'); // Redirect to register page on error
+                throw new Error('Registration failed');
+            }
+        })
+        .then(data => {
+            // Handle successful registration
+            console.log('Registration successful:', data);
+            navigate('/Login'); // Redirect to home page after registration
+        })
+        .catch(error => {
+            // Handle errors
+            console.error('Error:', error);
+        });
+    };
+
+    return (
+        <div className="register-container">
+            <h2>Register</h2>
+            <input
+                type="text" 
+                placeholder="Full Name" 
+                value={name} 
+                onChange={(e) => setName(e.target.value)}
+            />
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            
+            <button onClick={handleRegister}>Register</button>
+        </div>
+    );
+
+
+}
+
+export default Register;
